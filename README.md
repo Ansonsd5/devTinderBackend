56cadc4979c0e381ad48a77b45157b342a69a996
anson.dsouza <anson.dsouza@niveussolutions.com>
2025-04-25 00:42:12 +0530

    Basics of routing

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # devTinderBackend
-Crud apis
+# Episode1
+
+## Description
+A simple Node.js server using Express with two basic routes and Nodemon for development.
+
+## Getting Started
+
+### Prerequisites
+- Node.js
+- NPM
+
+### Installation
+
+1. Clone the repo or download the files.
+2. Run the following commands:
+
+```bash
+npm install
+
--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@ import  express  from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.get('/user', (req, res) => {
+    res.send({
+        firstName : "Anson",
+        lastName : "Dsouza",
+    })
   });
 
+app.post('/user',(req, res)=>{
+  console.log("Save data to DB");
+  res.send("Data saved to DB successfully|")
+})
 
-  app.listen(3001,()=>{console.log("App listening at 3001")})
+
+  app.listen(7777,()=>{console.log("App listening at 7777")})
