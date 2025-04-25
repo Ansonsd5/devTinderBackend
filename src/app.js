6ed70da6df6da2fbc5d727d4c700b4e0a705aae8
anson.dsouza <anson.dsouza@niveussolutions.com>
2025-04-25 18:53:11 +0530

    use of routing and next

--- src/app.js
+++ src/app.js
@@ -2,22 +2,17 @@ import  express  from "express";
 
 const app = express();
 
-app.get('/user', (req, res) => {
-    res.send({
-        firstName : "Anson",
-        lastName : "Dsouza",
-    })
-  });
-
-app.post('/user',(req, res)=>{
-  console.log("Save data to DB");
-  res.status(204).send({status : 204,data :"Data saved to DB successfully|"})
-})
-
-app.get('/user/:userId/userName/:passkey',(req, res)=>{
-console.log(req.params);
-res.send("Route ending with a")
-})
+app.use('/user',(req,res,next)=>{
+    console.log("user route is called");
+    next();
+    res.send("Response 1");
+},
+(req,res)=>{
+    console.log("/user second callback function");
+    res.send("Response from 2nd function");
+}
+)
 
 
-  app.listen(7777,()=>{console.log("App listening at 7777")})
+
+app.listen(7777,()=>{console.log("App listening at 7777")})
